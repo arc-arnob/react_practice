02b9234188915d20982e69e6dccdee8897b5a7c8
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2022-11-04 10:55:47 +0530

    Added First most Basic Component ever

--- getting_started_project/index.js
+++ getting_started_project/index.js
@@ -1,1 +1,23 @@
-ReactDOM.render(<h1>Hello Everyone</h1>, document.getElementById("root"));
+// Define a new Component here
+
+const NavBar = () => {
+    return (
+        <h1>Nav Bar</h1>
+    );
+};
+
+const MainContent = () => {
+    return(
+        <div>
+            This is the body
+        </div>
+    )
+}
+
+
+ReactDOM.render(
+    <div>
+        <NavBar />
+        <MainContent />
+    </div>
+    , document.getElementById("root"));
